2a5d677a903768e363dc71546cc585c8c54bec19
LuciferTheFirstt <dimonator67@gmail.com>
2019-12-09 16:25:12 +0300

    Source

---
+++ sources/Source.cpp
@@ -0,0 +1,104 @@
+#include "Header.h"
+
+void Cache::initbuffer(int n)
+{
+buffer = new unsigned int[n];
+
+for (unsigned int i = 0; i < n; i++)
+buffer[i] = rand() % n;
+}
+
+int Cache::KBtoSize(int kb)
+{
+return kb * 1024 / sizeof(int);
+}
+
+void Cache::DirectWay(int n)
+{
+for (int loop = 0; loop < LOOP; ++loop)
+{
+for (int i = 0; i < n; ++i)
+int v = buffer[i];
+}
+}
+
+void Cache::ReversWay(int n)
+{
+for (int loop = 0; loop < LOOP; ++loop)
+{
+for (int i = n - 1; i >= 0; --i)
+int v = buffer[i];
+}
+}
+
+void Cache::RandomWay(int n)
+{
+std::vector<int> v;
+
+for (int i = 0; i, n; ++i)
+v.push_back(buffer[i]);
+
+std::random_device rd;
+std::mt19937 g(rd());
+
+for (int loop = 0; loop < LOOP; ++loop)
+std::shuffle(v.begin(), v.end(), g);
+
+}
+
+Cache::Experiment* Cache::MakeTest(std::string travel)
+
+{
+
+std::cout << "investigation:" << std::endl;
+std::cout << "   travel variant: " << travel << std::endl;
+std::cout << "   experiments:" << std::endl;
+for (int i = 0; i < size; i++)
+{
+
+int n = KBtoSize(mas_sizes[i]);
+initbuffer(n);
+
+//прогрев
+for (int j = 0; j < n; j++)
+int a = buffer[rand() % n];
+
+auto start = std::chrono::high_resolution_clock::now();
+
+if (travel == "direct")
+{
+DirectWay(n);
+}
+
+else if (travel == "reverse")
+{
+ReversWay(n);
+}
+
+else {
+
+RandomWay(n);
+
+}
+
+auto finish = std::chrono::high_resolution_clock::now();
+
+
+//номер эксперемента
+std::cout << "   - experiment:" << std::endl;
+std::cout << "       number: " << (i + 1) << std::endl;
+
+//размер
+std::cout << "       input data:" << std::endl;
+std::cout << "         buffer size: " << mas_sizes[i] << "KB" << std::endl;
+
+//время
+std::cout << "       results:" << std::endl;
+std::cout << "         duration: " << (std::chrono::duration_cast<std::chrono::milliseconds>(finish - start).count() / LOOP) << "ms" << std::endl;
+
+}
+
+std::cout << std::endl;
+return(0);
+
+}
