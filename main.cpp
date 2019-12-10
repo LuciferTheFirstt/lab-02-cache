fa2ad53bf3b5736d2614b90891305a764ad44033
LuciferTheFirstt <dimonator67@gmail.com>
2019-12-11 00:06:19 +0300

    Ff

--- main.cpp
+++ main.cpp
@@ -1,14 +1,16 @@
 #include "Header.h"
 
+using namespace std;
+
 int main()
 {
-int min_kb, max_kb;
-std::string travel;
-min_kb = 128;
-max_kb = 6144;
+	int min_kb, max_kb;
+	string travel;
+	min_kb = 128;
+	max_kb = 6144;
 
-Cache cache(min_kb, max_kb);
-Cache::Experiment* direct = cache.MakeTest("direct");
-Cache::Experiment* reverse = cache.MakeTest("reverse");
-Cache::Experiment* random = cache.MakeTest("random");
+	Cache cache(min_kb, max_kb);
+	Cache::Experiment *direct = cache.MakeTest("direct");
+	Cache::Experiment *reverse = cache.MakeTest("reverse");
+	Cache::Experiment *random = cache.MakeTest("random");
 }
