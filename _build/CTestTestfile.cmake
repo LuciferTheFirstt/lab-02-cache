7fae610b47feaa134dc95817d27602c85ded5b0f
LuciferTheFirstt <dimonator67@gmail.com>
2019-12-12 13:53:20 +0300

    Cmake Travis

---
+++ _build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/luciferthefirstt/LuciferTheFirstt/workspace/workspace/projects/AL/lab02
+# Build directory: /home/luciferthefirstt/LuciferTheFirstt/workspace/workspace/projects/AL/lab02/_build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(CH "/home/luciferthefirstt/LuciferTheFirstt/workspace/workspace/projects/AL/lab02/_build/tests")
